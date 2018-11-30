fe8193b55bcc35712b8d22168a704b7b3af2a9db
Evan Hutchinson <evan.hutchinson@anthem.com>
2018-11-30 17:11:30 -0500

    Added rotate and snapToPosition

--- swipe/src/Deck.tsx
+++ swipe/src/Deck.tsx
@@ -3,7 +3,11 @@ import {
   View,
   Animated,
   PanResponder,
-  PanResponderInstance
+  PanResponderInstance,
+  Dimensions,
+  NativeScrollEvent,
+  PanResponderGestureState,
+  GestureResponderEvent
 } from "react-native";
 
 import { DeckData } from "./models";
@@ -20,35 +24,76 @@ class Deck extends Component<Props> {
   static defaultProps: Props;
   panResponder: PanResponderInstance;
   position: Animated.AnimatedValueXY;
+  WIDTH: number;
   constructor(props: Props) {
     super(props);
 
     this.position = new Animated.ValueXY();
     this.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: e => true,
-      onPanResponderMove: (evt, gestureState) => {
-        this.position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      onPanResponderMove: (evt, gesture) => {
+        this.position.setValue({
+          x: gesture.dx,
+          y: gesture.dy
+        });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (evt, gesture) => {
+        this.snapToPosition(evt, gesture);
+      }
     });
+
+    this.WIDTH = Dimensions.get("window").width;
   }
 
-  renderCards() {
+  getCardStyle(): any {
+    const { position, WIDTH } = this;
+    const rotate = position.x.interpolate({
+      inputRange: [-WIDTH, 0, WIDTH],
+      outputRange: ["-90deg", "0deg", "90deg"]
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  renderCards(): any {
     const { data, renderCard } = this.props;
-    return data.map(item => {
+    return data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.panResponder.panHandlers}
+          >
+            {renderCard(item)}
+          </Animated.View>
+        );
+      }
       return renderCard(item);
     });
   }
 
+  snapToPosition(
+    evt: GestureResponderEvent,
+    gesture: PanResponderGestureState
+  ): void {
+    if (Math.abs(gesture.dx) > 150) {
+      return Animated.spring(this.position, {
+        toValue: {
+          x: (this.WIDTH + 1) * (gesture.dx / Math.abs(gesture.dx)),
+          y: 0
+        }
+      }).start();
+    }
+    return Animated.spring(this.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
   render() {
-    return (
-      <Animated.View
-        style={this.position.getLayout()}
-        {...this.panResponder.panHandlers}
-      >
-        {this.renderCards()}
-      </Animated.View>
-    );
+    return <View>{this.renderCards()}</View>;
   }
 }
 
